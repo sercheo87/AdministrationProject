24fcdc88316ad274bdb17f31e6b18513d6fa244f
Sergio <chancaysergio@gmail.com>
2014-12-01 17:40:46 -0500

    Ajustes en entiy

--- ProjectAdminMng/JavaSource/com/project/entity/User.java
+++ ProjectAdminMng/JavaSource/com/project/entity/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "dbuser")
+@Table(name = "tbUser")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
