f6c81e650549970d4fa0b8a876300798321fe954
Sergio <chancaysergio@gmail.com>
2015-01-06 08:52:12 -0500

    Completando el crud del tipo de recurso y control de exepciones

---
+++ ProjectAdminMng/JavaSource/com/project/dto/MessageSeverity.java
@@ -0,0 +1,12 @@
+package com.project.dto;
+
+public enum MessageSeverity {
+	danger("danger"), info("info"), success("sucess"), warning("warning");
+
+	@SuppressWarnings("unused")
+	private String value;
+
+	MessageSeverity(String value) {
+		this.value = value;
+	}
+}
