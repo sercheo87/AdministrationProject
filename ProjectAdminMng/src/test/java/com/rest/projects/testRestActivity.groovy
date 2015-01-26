d366798cabfe65a637991ddfffd86ae02ea8fd1e
Sergio <chancaysergio@gmail.com>
2015-01-25 20:17:13 -0500

    Pruebas unitarias

--- ProjectAdminMng/src/test/java/com/rest/projects/testRestActivity.groovy
+++ ProjectAdminMng/src/test/java/com/rest/projects/testRestActivity.groovy
@@ -14,30 +14,16 @@ public class testRestActivity {
 
 	@Test
 	public void testCustomerResource() {
+		println 'GET ALL ACTIVITIES'
 		def http = new HTTPBuilder( 'http://127.0.0.1:1313/ProjectAdminMng/rest/' )
-
 		http.get( path: 'activity/getAll') { resp, json ->
-			println resp.status
-			println json
-
-			assert resp.status == 200
 			println "Got response: ${resp.status}"
 			println "Content-Type: ${resp.headers.'Content-Type'}"
 
-
-			def		builder2 = new JsonBuilder()
-			builder2 {
-				data json.data.collect {data ->
-					return {
-						data.each {key, value ->
-							"$key" value
-						}
-					}
-				}
-			}
-
-			println builder2.toPrettyString()
-			//states.data.each { println it.toString() }
+			assert resp.status == 200
+			JsonBuilder res= new restTools().convertToJson(json)
+			assert json.get('data').size()
+			println res.toString()
 		}
 	}
 }
